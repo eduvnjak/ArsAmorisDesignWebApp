1f6cc65a5a687714e453855071779488f51c3e83
Eman <eduvnjak1@etf.unsa.ba>
2023-11-03 19:30:00 +0100

    Tailwind installed

--- ars-amoris-design-app/src/Root.jsx
+++ ars-amoris-design-app/src/Root.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
-import './Root.css';
+//import './Root.css';
 import { Link, Outlet } from 'react-router-dom';
 import NavigationMenu from './components/NavigationMenu';
 import AuthenticationMenu from './components/AuthenticationMenu';
