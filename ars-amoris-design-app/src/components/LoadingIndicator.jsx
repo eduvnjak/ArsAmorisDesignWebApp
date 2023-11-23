511a112091993e7978d3dc5d362538fd6d87a04c
Eman <eduvnjak1@etf.unsa.ba>
2023-11-23 20:23:09 +0100

    Animated loading spinner

--- ars-amoris-design-app/src/components/LoadingIndicator.jsx
+++ ars-amoris-design-app/src/components/LoadingIndicator.jsx
@@ -1,3 +1,12 @@
 export default function LoadingIndicator() {
-	return <h1 className='p-3 text-white text-2xl text-center font-semibold'>Loading...</h1>;
+	// mozda prop za mijenjanje boje i/ili velicine
+	return (
+		<>
+			<svg width='48' height='48' viewBox='0 0 24 24' className='animate-spin mx-auto my-3 fill-blue-500' xmlns='http://www.w3.org/2000/svg'>
+				<path d='M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z' fill='#ffffff' />
+				<path d='M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z' opacity={0.6}/>
+			</svg>
+			<h1 className='p-3 text-white text-2xl text-center font-semibold'>Loading...</h1>;
+		</>
+	);
 }
