81146d2494aa9e79ccb092ffabf75a029ef1a66d
Eman <eduvnjak1@etf.unsa.ba>
2024-01-15 15:13:17 +0100

    Some component refactoring; dropdown element styling; proper focus/blur

--- ars-amoris-design-app/src/components/MultpleSelect.jsx
+++ ars-amoris-design-app/src/components/MultpleSelect.jsx
@@ -1,24 +1,39 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export default function MultipleSelect({ options, selectedOptions, onChange }) {
 	const spanRef = useRef(null);
+	const dropdownRef = useRef(null);
+	const hoveredRef = useRef(false);
+
 	const [isExpanded, setIsExpanded] = useState(false);
 
-	function handleLabelClick() {
-		spanRef.current.focus();
-	}
+	useEffect(() => {
+		if (dropdownRef.current != null) {
+			dropdownRef.current.style.width = `${spanRef.current.offsetWidth}px`;
+		}
+	}, [isExpanded]);
 
 	return (
 		<>
-			<span className='cursor-default' onClick={handleLabelClick}>
+			<span className='cursor-default' onClick={() => spanRef.current.focus()}>
 				Filtriraj po kategorijama:
 			</span>
-			<div className='cursor-default my-2 mx-1 text-left inline-block'>
+			<div
+				className='cursor-default my-2 mx-1 text-left inline-block'
+				onMouseEnter={() => {
+					hoveredRef.current = true;
+				}}
+				onMouseLeave={() => {
+					hoveredRef.current = false;
+				}}
+				onBlurCapture={() => {
+					if (!hoveredRef.current) setIsExpanded(false);
+				}}
+			>
 				<span
 					tabIndex={0}
 					className='transition-all duration-300 pl-2 pr-1 py-1 bg-white shadow-md focus:outline-none focus:ring focus:ring-blue-600'
 					onClick={() => setIsExpanded(!isExpanded)}
-					onBlur={() => setIsExpanded(false)}
 					onKeyDown={e => {
 						e.preventDefault();
 						if (e.code === 'Space') setIsExpanded(true);
@@ -27,54 +42,54 @@ export default function MultipleSelect({ options, selectedOptions, onChange }) {
 					ref={spanRef}
 				>
 					Odaberi kategorije &nbsp;&nbsp;
-					<svg
-						className='inline-block'
-						width='20px'
-						height='20px'
-						viewBox='-12 0 32 32'
-						fill='none'
-						xmlns='http://www.w3.org/2000/svg'
-					>
-						<g id='SVGRepo_iconCarrier'>
-							<path
-								d='M6 9L12 15L18 9'
-								stroke='#000000'
-								strokeWidth='3'
-								strokeLinecap='square'
-								strokeLinejoin='miter'
-							></path>
-						</g>
-					</svg>
+					<Chevron />
 				</span>
 				{isExpanded && (
-					<div className='absolute z-0'>
+					<div className='absolute z-0' ref={dropdownRef}>
 						{options.map(option => (
-							<div key={option.id} className='bg-white p-2 hover:text-white hover:bg-blue-600'>
-								<label className='hover:cursor-pointer'>
-									{option.name}{' '}
-									<input
-										className='hover:cursor-pointer'
-										type='checkbox'
-										checked={selectedOptions.includes(option.id)}
-										onChange={() => onChange(option.id)}
-									></input>
-								</label>
-							</div>
+							<DropDownElement
+								key={option.id}
+								name={option.name}
+								checked={selectedOptions.includes(option.id)}
+								onChange={() => onChange(option.id)}
+							/>
 						))}
-						<div className='bg-white p-2 hover:text-white hover:bg-blue-600'>
-							<label className='hover:cursor-pointer'>
-								Bez kategorije{' '}
-								<input
-									className='hover:cursor-pointer'
-									type='checkbox'
-									onChange={() => onChange('null')}
-									checked={selectedOptions.includes('null')}
-								></input>
-							</label>
-						</div>
+						<DropDownElement
+							name={'Bez kategorije'}
+							checked={selectedOptions.includes('null')}
+							onChange={() => onChange('null')}
+						/>
 					</div>
 				)}
 			</div>
 		</>
+	);
+}
+// nisu exportovane, komponente izdvojene radi preglednosti
+function DropDownElement({ name, checked, onChange }) {
+	return (
+		<div className='bg-white p-2 hover:text-white hover:bg-blue-600'>
+			<label className='hover:cursor-pointer flex'>
+				<span className='flex-grow inline-block'>{name}</span>
+				<input className='hover:cursor-pointer' type='checkbox' checked={checked} onChange={onChange}></input>
+			</label>
+		</div>
+	);
+}
+
+function Chevron() {
+	return (
+		<svg
+			className='inline-block'
+			width='20px'
+			height='20px'
+			viewBox='-12 0 32 32'
+			fill='none'
+			xmlns='http://www.w3.org/2000/svg'
+		>
+			<g id='SVGRepo_iconCarrier'>
+				<path d='M6 9L12 15L18 9' stroke='#000000' strokeWidth='3' strokeLinecap='square' strokeLinejoin='miter'></path>
+			</g>
+		</svg>
 	);
 }
