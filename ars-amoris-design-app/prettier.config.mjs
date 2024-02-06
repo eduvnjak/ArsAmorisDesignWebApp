4cd450ec1717d26c5493345e4f2093b9a5d30bb3
Eman <eduvnjak1@etf.unsa.ba>
2024-02-06 17:46:34 +0100

    Prettier config file with tailwind plugin

---
+++ ars-amoris-design-app/prettier.config.mjs
@@ -0,0 +1,9 @@
+// prettier.config.js
+export default {
+    "arrowParens": "avoid",
+    "jsxSingleQuote": true,
+    "proseWrap": "never",
+    "singleQuote": true,
+    "useTabs": true,
+    "plugins": ['prettier-plugin-tailwindcss']
+}
