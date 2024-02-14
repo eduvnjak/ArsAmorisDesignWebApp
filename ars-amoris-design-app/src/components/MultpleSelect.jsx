1cc6120fb73603d68c959c199a7e290c513de2e1
Eman <eduvnjak1@etf.unsa.ba>
2024-02-14 20:09:02 +0100

    Added border to MultipleSelect (also ordered classes)

--- ars-amoris-design-app/src/components/MultpleSelect.jsx
+++ ars-amoris-design-app/src/components/MultpleSelect.jsx
@@ -19,7 +19,7 @@ export default function MultipleSelect({ options, selectedOptions, onChange }) {
 				Filtriraj po kategorijama:
 			</span>
 			<div
-				className='cursor-default m-2 text-left inline-block'
+				className='m-2 inline-block cursor-default text-left'
 				onMouseEnter={() => {
 					hoveredRef.current = true;
 				}}
@@ -32,7 +32,7 @@ export default function MultipleSelect({ options, selectedOptions, onChange }) {
 			>
 				<span
 					tabIndex={0} // ovaj span srediti
-					className='transition-all duration-300 pl-2 pr-1 py-1 bg-white shadow-md focus:outline-none focus:ring focus:ring-blue-600'
+					className='bg-white py-1 pl-2 pr-1 shadow-md transition-all duration-300 focus:outline-none focus:ring focus:ring-blue-600'
 					onClick={() => setIsExpanded(!isExpanded)}
 					onKeyDown={e => {
 						e.preventDefault();
@@ -68,10 +68,15 @@ export default function MultipleSelect({ options, selectedOptions, onChange }) {
 // nisu exportovane, komponente izdvojene radi preglednosti
 function DropDownElement({ name, checked, onChange }) {
 	return (
-		<div className='bg-white p-2 hover:text-white hover:bg-blue-600'>
-			<label className='hover:cursor-pointer flex'>
-				<span className='flex-grow inline-block'>{name}</span>
-				<input className='hover:cursor-pointer' type='checkbox' checked={checked} onChange={onChange}></input>
+		<div className='border-x bg-white p-2 first:border-t last:border-b hover:bg-blue-600 hover:text-white'>
+			<label className='flex hover:cursor-pointer'>
+				<span className='inline-block flex-grow'>{name}</span>
+				<input
+					className='hover:cursor-pointer'
+					type='checkbox'
+					checked={checked}
+					onChange={onChange}
+				></input>
 			</label>
 		</div>
 	);
@@ -88,7 +93,13 @@ function Chevron() {
 			xmlns='http://www.w3.org/2000/svg'
 		>
 			<g id='SVGRepo_iconCarrier'>
-				<path d='M6 9L12 15L18 9' stroke='#000000' strokeWidth='3' strokeLinecap='square' strokeLinejoin='miter'></path>
+				<path
+					d='M6 9L12 15L18 9'
+					stroke='#000000'
+					strokeWidth='3'
+					strokeLinecap='square'
+					strokeLinejoin='miter'
+				></path>
 			</g>
 		</svg>
 	);
