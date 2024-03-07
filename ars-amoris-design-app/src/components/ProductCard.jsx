5b31c85885103f1c70f0b5caa3d751aa51ad1da2
Eman <eduvnjak1@etf.unsa.ba>
2024-03-07 19:04:41 +0100

    Product liking on FE

--- ars-amoris-design-app/src/components/ProductCard.jsx
+++ ars-amoris-design-app/src/components/ProductCard.jsx
@@ -1,14 +1,55 @@
 /* eslint-disable react/prop-types */
 import { useEffect, useRef } from 'react';
+import useAxios from '../api/useAxios';
+import { useAuth } from '../contexts/AuthContext';
 
 export default function ProductCard({ children, product }) {
-	const { name, price, imageUrl, categoryName, likeCount, liked } = product;
+	const { id, name, price, imageUrl, categoryName, likeCount, liked } = product;
+	// if (liked != isLiked) setIsLiked(liked); // mirroring prop in state - oprez ! ovo je zeznuto totalno
 	const nameRef = useRef(null);
+	const heartParentRef = useRef(null);
+	const likeCountRef = useRef(null);
+
+	const axiosInstance = useAxios();
+	const { isAuthenticated } = useAuth();
+
 	useEffect(() => {
 		if (nameRef.current !== null && nameRef.current.scrollWidth > 296) {
 			nameRef.current.classList.add('name-tooltip');
 		}
 	}, []);
+
+	async function handleHeartClick(event) {
+		// neki debouncing ?
+		// ovo je optimistic
+		event.stopPropagation();
+		if (heartParentRef.current) {
+			if (isAuthenticated) {
+				const endpoint = `Products/${id}/Like`;
+				// izbjegnuto koristenje state zbog problema sa prop mirroringom
+				try {
+					if (heartParentRef.current.getAttribute('data-liked') === 'true') {
+						await axiosInstance.delete(endpoint);
+						heartParentRef.current.setAttribute('data-liked', 'false');
+						likeCountRef.current.textContent =
+							parseInt(likeCountRef.current.textContent) - 1;
+					} else {
+						await axiosInstance.post(endpoint);
+						heartParentRef.current.setAttribute('data-liked', 'true');
+						likeCountRef.current.textContent =
+							parseInt(likeCountRef.current.textContent) + 1;
+					}
+				} catch (error) {
+					console.log(error);
+				}
+			} else {
+				heartParentRef.current.classList.add('tooltip');
+				setTimeout(() => {
+					heartParentRef.current.classList.remove('tooltip');
+				}, 3000);
+			}
+		}
+	}
 
 	const duration = Math.random() * 500 + 300;
 	return (
@@ -41,8 +82,18 @@ export default function ProductCard({ children, product }) {
 					<span>{categoryName ?? 'Nekategorisan'}</span>
 					<span className='text-lg font-bold'>{price} BAM</span>
 				</div>
-				<span data-liked={liked} className='px-2 font-bold'>
-					<HeartIcon /> {likeCount}
+				<span className='px-2 font-bold'>
+					<span
+						data-liked={liked}
+						ref={heartParentRef}
+						className='relative inline-block after:absolute after:left-[50%] after:-z-10 after:inline-block after:-translate-x-[50%] after:whitespace-nowrap after:rounded-sm after:bg-slate-500 after:px-4 after:py-2 after:text-sm after:font-normal after:text-slate-50 after:opacity-0 after:transition-all after:duration-300 after:ease-linear after:content-["Molimo_prijavite_se"] [&.tooltip]:after:z-10 [&.tooltip]:after:-translate-y-[110%] [&.tooltip]:after:opacity-100'
+						onClickCapture={handleHeartClick}
+					>
+						<HeartIcon />
+					</span>
+					<span ref={likeCountRef} className='px-1'>
+						{likeCount}
+					</span>
 				</span>
 				<div className='flex flex-col gap-4 sm:flex-row'>{children}</div>
 			</div>
@@ -57,7 +108,7 @@ function HeartIcon() {
 			fill='none'
 			viewBox='0 0 24 24'
 			strokeWidth={2}
-			className='inline-block h-6 w-6 stroke-red-600 [[data-liked="true"]_&]:fill-red-600'
+			className='inline-block h-6 w-6 stroke-red-600 transition duration-300 ease-in hover:cursor-pointer [[data-liked="false"]_&]:fill-slate-50 [[data-liked="false"]_&]:hover:fill-red-200 [[data-liked="true"]_&]:fill-red-600'
 		>
 			<path
 				strokeLinecap='round'
