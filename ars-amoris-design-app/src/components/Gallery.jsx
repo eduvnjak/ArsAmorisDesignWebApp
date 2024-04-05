4f105371a72bfa9a0e7c99c0d1a54b40f8237461
Eman <eduvnjak1@etf.unsa.ba>
2024-04-05 14:47:21 +0200

    Gallery component with scroll

--- ars-amoris-design-app/src/components/Gallery.jsx
+++ ars-amoris-design-app/src/components/Gallery.jsx
@@ -1,18 +1,44 @@
 /* eslint-disable react/prop-types */
-import { useState } from 'react';
+import { useState, useRef } from 'react';
+import { flushSync } from 'react-dom';
 
 export default function Gallery({ images }) {
 	const [currentIndex, setCurrentIndex] = useState(0);
+	const currentImageRef = useRef(null);
 
 	function showPrevious() {
-		if (currentIndex !== 0) setCurrentIndex(currentIndex - 1);
+		if (currentIndex !== 0) {
+			flushSync(() => setCurrentIndex(currentIndex - 1));
+			currentImageRef.current.scrollIntoView({
+				behavior: 'smooth',
+				inline: 'center',
+				block: 'center',
+			});
+		}
 	}
 	function showNext() {
-		if (currentIndex !== images.length - 1) setCurrentIndex(currentIndex + 1);
+		if (currentIndex !== images.length - 1) {
+			flushSync(() => setCurrentIndex(currentIndex + 1));
+			currentImageRef.current.scrollIntoView({
+				behavior: 'smooth',
+				inline: 'center',
+				block: 'center',
+			});
+		}
 	}
 	return (
-		<div className='relative flex h-full w-full justify-center'>
-			<img src={images[currentIndex]} className='object-cover'></img>
+		<div className='relative h-full w-full'>
+			<div className='h-full overflow-hidden whitespace-nowrap'>
+				{images.map((image, index) => (
+					<img
+						key={index}
+						ref={index === currentIndex ? currentImageRef : null}
+						src={image}
+						alt={'image ' + index}
+						className='mx-auto inline h-full w-full object-scale-down'
+					></img>
+				))}
+			</div>
 			{images.length > 1 && (
 				<>
 					<button
@@ -71,74 +97,3 @@ function ChevronRight() {
 		</svg>
 	);
 }
-
-// import { useRef } from 'react';
-
-// export function CatFriends() {
-//   const itemsRef = useRef(null);
-
-//   function scrollToId(itemId) {
-//     const map = getMap();
-//     const node = map.get(itemId);
-//     node.scrollIntoView({
-//       behavior: 'smooth',
-//       block: 'nearest',
-//       inline: 'center'
-//     });
-//   }
-
-//   function getMap() {
-//     if (!itemsRef.current) {
-//       // Initialize the Map on first usage.
-//       itemsRef.current = new Map();
-//     }
-//     return itemsRef.current;
-//   }
-
-//   return (
-//     <>
-//       <nav>
-//         <button onClick={() => scrollToId(0)}>
-//           Tom
-//         </button>
-//         <button onClick={() => scrollToId(5)}>
-//           Maru
-//         </button>
-//         <button onClick={() => scrollToId(9)}>
-//           Jellylorum
-//         </button>
-//       </nav>
-//       <div>
-//         <ul>
-//           {catList.map(cat => (
-//             <li
-//               key={cat.id}
-//               ref={(node) => {
-//                 const map = getMap();
-//                 if (node) {
-//                   map.set(cat.id, node);
-//                 } else {
-//                   map.delete(cat.id);
-//                 }
-//               }}
-//             >
-//               <img
-//                 src={cat.imageUrl}
-//                 alt={'Cat #' + cat.id}
-//               />
-//             </li>
-//           ))}
-//         </ul>
-//       </div>
-//     </>
-//   );
-// }
-
-// const catList = [];
-// for (let i = 0; i < 10; i++) {
-//   catList.push({
-//     id: i,
-//     imageUrl: 'https://placekitten.com/250/200?image=' + i
-//   });
-// }
-
